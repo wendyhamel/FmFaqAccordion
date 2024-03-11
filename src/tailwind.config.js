489e94c76b4df50d9811c7439ddf5c66f9208e6f
wendyhamel <whamel@it4web.nl>
2024-03-11 18:33:07 +0100

    styling mobile

---
+++ src/tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+	theme: {
+		fontFamily: {
+			'sans': ['"Work Sans"', 'sans-serif'],
+		},
+		extend: {
+			colors: {
+				'white' : 'hsl(0, 0%, 100%)',
+				'pink' : 'hsl(275, 100%, 97%)',
+				'purple' : '#AD28EB',
+				'purple-gray' : 'hsl(292, 16%, 49%)',
+				'purple-dark' : 'hsl(292, 42%, 14%)',
+			}
+		}
+	}
+}
